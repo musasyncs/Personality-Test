26c940a4d7f2edb4ae12b4ce9f99f2fff21b1d66
musasyncs <rubato.cw@gmail.com>
2021-08-06 10:47:56 +0800

    Initial Commit

---
+++ PersonalityTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityTest
+//
+//  Created by Ewen on 2021/8/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
